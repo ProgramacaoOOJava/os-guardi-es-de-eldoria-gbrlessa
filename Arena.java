6b2966fedde5d3eb4fb29f854c956db6132bc918
gbrlessa <102160982+gbrlessa@users.noreply.github.com>
2026-08-24 14:29:51 +0000

    Finalização dos desafios extras - Implementação da Arena.

--- Arena.java
+++ Arena.java
@@ -1,5 +1,14 @@
+import java.util.List;
+
 public class Arena {
     public void batalharGrupos(Grupo g1, Grupo g2) {
-        
+        List<Personagem> time1 = g1.getMembros();
+        List<Personagem> time2 = g2.getMembros();
+
+        int rounds = Math.min(time1.size(), time2.size());
+
+        for (int i = 0; i < rounds ; i++) {
+            g1.batalhar(time1.get(i), time2.get(i));
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -2,22 +2,33 @@
 public class Main {
     public static void main(String[] args) {
 
-        Grupo grupo1 = new Grupo ("Magos");
-        Grupo grupo2 = new Grupo ("Guerreiros");
+        // Criação dos grupos
+        Grupo magos = new Grupo ("Magos");
+        Grupo guerreiros = new Grupo ("Guerreiros");
+
+        //Criação dos personagens
         Mago drake = new Mago("Drake", "Mago", 240, 350, 360.0, 120.0);
         Mago jorge = new Mago("Jorge", "Mago", 103, 150, 170.0, 40.0);
         Guerreiro dory = new Guerreiro("Dory", "Guerreiro", 290, 890, 210.0, 190.0);
         Guerreiro claudia = new Guerreiro("Claudia", "Guerreiro", 130, 680, 150.0, 160.0);
 
-        grupo1.adicionarMembro(drake);
-        grupo1.adicionarMembro(jorge);
-        grupo2.adicionarMembro(dory);
-        grupo2.adicionarMembro(claudia);
+        //Adicionando os personagens aos grupos
+        magos.adicionarMembro(drake);
+        magos.adicionarMembro(jorge);
+        guerreiros.adicionarMembro(dory);
+        guerreiros.adicionarMembro(claudia);
 
-        grupo1.ordenarMembrosPorNivel();
-        grupo2.ordenarMembrosPorNivel();
+        //Ordenando os grupos por nível
+        magos.ordenarMembrosPorNivel();
+        guerreiros.ordenarMembrosPorNivel();
         
-        grupo1.listarMembros();
-        grupo2.listarMembros();
+        //Lista os membros
+        magos.listarMembros();
+        guerreiros.listarMembros();
+
+        //Realiza batalha dos grupos
+        System.out.println("\n --- Início da Batalha ---");
+        Arena arena = new Arena();
+        arena.batalharGrupos(magos, guerreiros);
     }
 }
